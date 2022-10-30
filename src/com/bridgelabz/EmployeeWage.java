83a76ab8d321824c828c6f2ef7a26b9fa17580d9
AnjaliRawke <anjalirawke21@gmail.com>
2022-10-30 21:51:00 +0530

    Added:UC6 Calculate Wages till a condition of total working hours or days is reached for a month

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,36 +1,42 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
- public static void main(String[] args) {
-	//constants
-	 int IS_FULL_TIME = 1;
-	 int IS_PART_TIME=2;
-	 int EMP_RATE_PER_HOUR = 20;
-	 int NUM_OF_WORKING_DAYS=20;
-	 //variables
-	 int empHrs = 0;
-	 int empWage = 0;
-	 int MonthlyWage;
-	      //Computation
-	 int empCheck =(int) (Math.floor(Math.random()*10)%3);
-	switch(empCheck)
-	{
-	case 1:
-		empHrs=8;
-		break;
-	case 2:
-		empHrs=4;
-		break;
-	case 3:
-		empHrs=0;
-		break;
-	}
-	 empWage = empHrs * EMP_RATE_PER_HOUR;
-	System.out.println("Daily emp wage: "+empWage);
-	MonthlyWage=empWage*NUM_OF_WORKING_DAYS;
-	System.out.println("Monthly emp Wage: "+MonthlyWage);
- }
-		 
+	public static void main(String[] args) {
+		//constants
+		 int IS_FULL_TIME = 1;
+		 int IS_PART_TIME=2;
+		 int EMP_RATE_PER_HOUR = 20;
+		 int NUM_OF_WORKING_DAYS=20;
+		 //variables
+		 int empHrs = 0;
+		 int empWage = 0;
+		 int MonthlyWage;
+		 int TotalempHrs=0;
+		 int maxHrsInMonth=100;
+		 int TotalWorkingDays=0;
+		      //Computation
+		while(TotalempHrs < maxHrsInMonth && TotalWorkingDays < NUM_OF_WORKING_DAYS) {
+		    TotalWorkingDays++;
+			int empCheck =(int) (Math.floor(Math.random()*10)%3);
+		switch(empCheck)
+		{
+		case 1:
+			empHrs=8;
+			break;
+		case 2:
+			empHrs=4;
+			break;
+		default:
+			empHrs=0;
+			break;
+		}
+		TotalempHrs=TotalempHrs+empHrs;
 	 }
+		System.out.println("TotalEmpHrs: "+TotalempHrs);
+		empWage=TotalempHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Monthly Wage: "+empWage);
+	}
+}
+		  
 	
 
